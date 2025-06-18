f7544ee90ce23a39d500f7356341ec3c9b146db6
MattyAtown <mattauton22@gmail.com>
2025-06-19 02:00:53 +0700

    Update app.py

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ import feedparser
 app = Flask(__name__)
 
 app.config['MAIL_SERVER'] = os.getenv('MAIL_SERVER')
-app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT'))
+app.config['MAIL_PORT'] = int(os.getenv('MAIL_PORT', 587))
 app.config['MAIL_USE_TLS'] = os.getenv('MAIL_USE_TLS') == 'True'
 app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
 app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
